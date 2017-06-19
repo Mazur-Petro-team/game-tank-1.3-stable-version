b57b35e347b98c696a22711811233aa378cbc9be
RostikMoroziuk <rmorozyukua@gmail.com>
2017-06-19 17:52:24 +0300

    Add control for two players

--- js/index.js
+++ js/index.js
@@ -38,14 +38,14 @@ $(function () {
 
     function changeTime() {
       var current = Date.now();
-      var timeOver = parseInt((endTime - current)/1000); //seconds
-      if(timeOver <= 0) {
+      var timeOver = parseInt((endTime - current) / 1000); //seconds
+      if (timeOver <= 0) {
         clearInterval(timer);
         gameOver();
       }
-      var minutes = parseInt(timeOver/60);
-      var seconds = timeOver%60;
-      if(seconds < 10) {
+      var minutes = parseInt(timeOver / 60);
+      var seconds = timeOver % 60;
+      if (seconds < 10) {
         seconds = "0" + seconds;
       }
       $('.timer .timer-minutes').text(minutes);
@@ -54,47 +54,117 @@ $(function () {
   }
 
   function keyControls() {
+    var KeyCode_LEFT_1 = 37;
+    var KeyCode_UP_1 = 38;
+    var KeyCode_RIGHT_1 = 39;
+    var KeyCode_DOWN_1 = 40;
+    var KeyCode_SHOOT_1 = 13; //enter
+    var KeyCode_LEFT_2 = 65; //a
+    var KeyCode_UP_2 = 87; //w
+    var KeyCode_RIGHT_2 = 68; //d
+    var KeyCode_DOWN_2 = 83; //s
+    var KeyCode_SHOOT_2 = 32; //space
+    var KeyCode_CTRL = 17;
+    var KeyCode_Z = 90;
+    var KeyCode_G = 71;
+    var STEP = 5; //px per 1 step of moving
+
+    var player1Tank = $('.tank[data-player="1"]');
+    var player2Tank = $('.tank[data-player="2"]');
+
+    //tank moving
+    //rotate tank left
+    function moveLeft(tank) {
+      var tankPosition = (parseInt(tank.css("left")) - 5) + "px";
+      tank.css({
+        'left': tankPosition,
+        'transform': 'rotate(180deg)'
+      });
+    }
+
+    //rotate tank right
+    function moveRight(tank) {
+      var tankPosition = (parseInt(tank.css("left")) + 5) + "px";
+      tank.css({
+        'left': tankPosition,
+        'transform': 'rotate(0deg)'
+      });
+    }
+
+    //rotate tank up
+    function moveUp(tank) {
+      var tankPosition = (parseInt(tank.css("top")) - 5) + 'px';
+      tank.css({
+        'transform': 'rotate(-90deg)',
+        'top': tankPosition
+      });
+    }
+
+    //rotate tank down
+    function moveDown(tank) {
+      var tankPosition = (parseInt(tank.css("top")) + 5) + 'px';
+      tank.css({
+        'transform': 'rotate(90deg)',
+        'top': tankPosition
+      });
+    }
+
+    function shoot(tank) {
+      var bullet = $('<div class="tank-shoot-bullet" />');
+      $(tank).append(bullet);
+      setTimeout(function() {
+        bullet.remove();
+      }, 600)
+    }
+
     document.body.onkeydown = function (e) {
       var el = document.getElementById('tank');
 
-      var KeyCode_LEFT = 37;
-      var KeyCode_UP = 38;
-      var KeyCode_RIGHT = 39;
-      var KeyCode_DOWN = 40;
-      var KeyCode_SPACE = 32;
-      var KeyCode_CTRL = 17;
-      var KeyCode_Z = 90;
-      var KeyCode_G = 71;
-
-      if (e.keyCode == KeyCode_LEFT) {
-        el.style.left = (parseInt(el.style.left) - 5) + 'px';
-        document.getElementById('tank').style.transform = 'rotate(180deg)';
-      } else if (e.keyCode == KeyCode_RIGHT) {
-        el.style.left = (parseInt(el.style.left) + 5) + 'px';
-        document.getElementById('tank').style.transform = 'rotate(0deg)';
-      } else if (e.keyCode == KeyCode_UP) {
-        el.style.top = (parseInt(el.style.top) - 5) + 'px';
-        document.getElementById('tank').style.transform = 'rotate(-90deg)';
-      } else if (e.keyCode == KeyCode_DOWN) {
-        el.style.top = (parseInt(el.style.top) + 5) + 'px';
-        document.getElementById('tank').style.transform = 'rotate(90deg)';
-      } else if (e.keyCode == KeyCode_SPACE) {
-        shoot();
-        setTimeout(function () {
-          $('.tank-shoot-bullet').remove();
-        }, 590);
-      } else if (e.keyCode == KeyCode_CTRL) {
-        deatsh();
-        setTimeout(function () {
-          $('.tank').remove();
-          $('#tank').remove();
-          $('<div class="death-of-tank" />').remove();
-        }, 2000);
-      } else if (e.keyCode == KeyCode_Z) {
-        neveTank();
-      } else if (e.keyCode == KeyCode_G) {
-        gameOver();
+      switch (e.keyCode) {
+        case KeyCode_LEFT_1:
+          moveLeft(player1Tank);
+          break;
+        case KeyCode_RIGHT_1:
+          moveRight(player1Tank);
+          break;
+        case KeyCode_UP_1:
+          moveUp(player1Tank);
+          break;
+        case KeyCode_DOWN_1:
+          moveDown(player1Tank);
+          break;
+        case KeyCode_LEFT_2:
+          moveLeft(player2Tank);
+          break;
+        case KeyCode_RIGHT_2:
+          moveRight(player2Tank);
+          break;
+        case KeyCode_UP_2:
+          moveUp(player2Tank);
+          break;
+        case KeyCode_DOWN_2:
+          moveDown(player2Tank);
+          break;
+        case KeyCode_SHOOT_1:
+          shoot(player1Tank);
+          break;
+        case KeyCode_SHOOT_2:
+          shoot(player2Tank);
+          break;
       }
+
+      // } else if (e.keyCode == KeyCode_CTRL) {
+      //   deatsh();
+      //   setTimeout(function () {
+      //     $('.tank').remove();
+      //     $('#tank').remove();
+      //     $('<div class="death-of-tank" />').remove();
+      //   }, 2000);
+      // } else if (e.keyCode == KeyCode_Z) {
+      //   neveTank();
+      // } else if (e.keyCode == KeyCode_G) {
+      //   gameOver();
+      // }
     };
   }
 
@@ -112,13 +182,19 @@ $(function () {
     $('.death').append(fireball);
   }
 
-  function shoot() {
-    var bullet = $('<div class="tank-shoot-bullet" />');
-    $('#tank').append(bullet);
-  }
-
   function TankPosition() {
-    document.getElementById('tank').style.top = '0px',
-      document.getElementById('tank').style.left = '0px';
+    //show
+    $('.tank').removeClass('hide');
+    //first tank
+    $('.tank[data-player="1"').css({
+      'top': '0px',
+      'left': '100px'
+    });
+    //second
+    $('.tank[data-player="2"').css({
+      'top': '390px',
+      'left': '980px',
+      'transform': 'rotate(180deg)'
+    });
   }
 });
